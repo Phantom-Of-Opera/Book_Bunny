3c26a7c70eea0700a47a3eeb875f10e7681d6a46
Olivier Terver <phantom@Oliviers-MacBook-Pro.local>
2025-03-24 01:53:53 -0400

    book search with cover

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ import * as fs from "node:fs";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import pg from "pg";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const apiSearch = "https://openlibrary.org/search.json";
+
 const fileBlogs = "./public/files/blogs.json";
 var selectBlog = null;
 
@@ -30,48 +33,79 @@ app.get("/", (req, res) => {
 	});
 });
 
-app.get("/blog", (req, res) => {
-	res.render("blog.ejs", {
-		page: "blog",
-		setAuthor: selectBlog[0].blogAuthor,
-		setTitle: selectBlog[0].blogTitle,
-		setBlogText: selectBlog[0].blogText,
-	});
-});
+// app.get("/blog", (req, res) => {
+// 	res.render("blog.ejs", {
+// 		page: "blog",
+// 		setAuthor: selectBlog[0].blogAuthor,
+// 		setTitle: selectBlog[0].blogTitle,
+// 		setBlogText: selectBlog[0].blogText,
+// 	});
+// });
 
 app.post("/tab", (req, res) => {
 	console.log(req.body);
-	res.send("OK");
+	switch (req.body.tab) {
+		case "New Book":
+			res.render("newBook.ejs");
+			break;
+
+		case "Show Library":
+			res.redirect("/");
+			break;
+
+		default:
+			res.send("OK");
+			break;
+	}
 });
 
-app.post("/select", (req, res) => {
-	selectBlog = selectFromFile(req.body.key);
-	res.send("OK");
+app.post("/search", async (req, res) => {
+	try {
+		console.log(req.body);
+
+		let searchString = new URLSearchParams({
+			title: req.body.searchTitle,
+		});
+		let searchURL =
+			apiSearch + "?" + searchString.toString().replace(/%20/g, "+");
+
+		const response = await axios.get(searchURL);
+		const result = response.data;
+
+		res.json(result);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Something went wrong" });
+	}
 });
 
-app.post("/new", (req, res) => {
-	selectBlog = [
-		{
-			blogAuthor: "Your name",
-			blogTitle: "Title of your blog",
-			blogText: "Your blog text",
-		},
-	];
-	res.send("OK");
-});
+// app.post("/select", (req, res) => {
+// 	selectBlog = selectFromFile(req.body.key);
+// });
 
-app.post("/submit", (req, res) => {
-	var newBlog = {
-		blogAuthor: req.body.author,
-		blogTitle: req.body.title,
-		blogText: req.body.blogText,
-		blogKey: req.body.title + "|" + req.body.author,
-		blogDate: new Date(),
-	};
+// app.post("/new", (req, res) => {
+// 	selectBlog = [
+// 		{
+// 			blogAuthor: "Your name",
+// 			blogTitle: "Title of your blog",
+// 			blogText: "Your blog text",
+// 		},
+// 	];
+// 	res.send("OK");
+// });
 
-	addToFile(newBlog);
-	res.redirect("/");
-});
+// app.post("/submit", (req, res) => {
+// 	var newBlog = {
+// 		blogAuthor: req.body.author,
+// 		blogTitle: req.body.title,
+// 		blogText: req.body.blogText,
+// 		blogKey: req.body.title + "|" + req.body.author,
+// 		blogDate: new Date(),
+// 	};
+
+// 	addToFile(newBlog);
+// 	res.redirect("/");
+// });
 
 app.post("/delete", (req, res) => {
 	removeFromFile(req.body.key);
